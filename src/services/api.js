1c55726c51acb0151f0004e505553cea03ec8753
Mário Barbosa <mariogomes@MacBook-Pro-de-Mario.local>
2019-10-10 14:10:08 -0300

    feature: Configuring socket.io

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://aircnc-back-end.herokuapp.com/'
+    baseURL: 'https://aircnc-back-end.herokuapp.com'
 })
 
 export default api;
